1b1f980c6ab1431cf95a563c1de9bdd6f256990c
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-04-26 09:01:25 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,5 +10,26 @@ app.use(express.json());
 
 // GET endpoint for sending the products to client by id
 //// Endpoint - /api/v1/products/:id
+app.get("/api/v1/products/:id", (req, res) => {
+    const ix = req.params.id;
+    console.log(ix);
+    let val = products.find((item) => {
+        return item.id == ix;
+    });
+
+    if (val) {
+        console.log(val);
+        res.status(200).json({
+            status: "success",
+            message: "Product fetched successfully",
+            data: {
+                product: val
+            }
+        });
+    } 
+    else {
+        res.status(404).json({ status: "failed", message: "Product not found!" });
+    }
+});
 
 module.exports = app;
